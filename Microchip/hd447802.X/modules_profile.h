25f1d14c283cc8253c1576ddeb1aa87b1f9ec6ab
Diego Perez <diego.perez@hotboards.org>
2014-04-17 22:25:21 -0700

    se borran archivos profile en desuso

--- Microchip/hd447802.X/modules_profile.h
+++
@@ -1,9 +0,0 @@
-/* 
-Define en este archivo las diferentes opciones de configuracion existentes en tus drivers de nivel 
-intermdedio dentro de la carpeta "middleware"
- */
-#ifndef __MODULES_PROFILE_H__
-#define	__MODULES_PROFILE_H__
-
-    
-#endif	
