0c9f44d6346ed124d2e4e6011d990a6ae273e02f
Diego Perez <diego.perez@hotboards.org>
2014-04-17 23:50:15 -0700

    se agrega tercer ejemplo del driver 7segments

---
+++ Microchip/7segments_3.X/hardware_profile.h
@@ -0,0 +1,21 @@
+/* 
+Define en este archivo las diferentes opciones de configuracion existentes en tus drivers de bajo
+nivel dentro de la carpeta "bsp"
+ */
+#ifndef __HARDWARE_PROFILE_H__
+#define	__HARDWARE_PROFILE_H__
+
+    #define _7SEGMENTS_DIGI_N                   3       /*3 digitos*/
+    #define _7SEGMENTS_MODE                     1       /*anodo comun*/
+    #define _7SEGMENTS_PORT                     GPIOS_PORTB /*puerto b como bus de datos*/
+    /*pines que controlan los comunes*/
+    #define _7SEGMENTS_DIGI0_P                  GPIOS_PORTD /*primer display*/
+    #define _7SEGMENTS_DIGI0_B                  7
+
+    #define _7SEGMENTS_DIGI1_P                  GPIOS_PORTD /*segundo display*/
+    #define _7SEGMENTS_DIGI1_B                  6
+
+    #define _7SEGMENTS_DIGI2_P                  GPIOS_PORTD /*tercero display*/
+    #define _7SEGMENTS_DIGI2_B                  5
+
+#endif	
---
+++ Microchip/7segments_3.X/main.c
@@ -0,0 +1,63 @@
+/*
+ * Se despliega el voltaje leido en el canal 3 del adc en tres displays, cada 100ms
+ * se toma el puerto B como bus de datos y los pines display1 = D7, display2 = D6,
+ * display4 = D4 para los comunes. los displays son positivo comun. Revisa el archivo hardware_profile.h
+ * El micro tiene una velocidad de 12MHz, cristal externo
+ */
+
+#include <p18cxxx.h>
+#include "vectors.h"
+#include "gpios/gpios.h"
+#include "swtimers/swtimers.h"
+#include "system/system.h"
+#include "adc/adc.h"
+#include "7segments/_7segments.h"
+
+
+#pragma code
+void main(void)
+{
+    _U08 u8Adc = 0;
+    _U08 u8Val;
+    _U16 u16Volts;
+
+    ANCON0 = 0xff;
+    ANCON1 = 0xff;
+
+    Adc_Init(ADC_8BITS);
+    Timers_Init();                      /*inicializamos el driver para genere una interrupcion cada 5ms*/
+    _7segments_Init();                  /*configuramos los pines como salidas*/
+  
+    __ENABLE_INTERRUPTS();   /*se habilitan las interrupciones globales con prioridad*/
+
+    while (1)
+    {
+        if(Timers_u16GetTime(0) == 0)/*preguntamos si la interrupcion decrmento hasta llegar a 0 el canal 0*/
+        {
+            Timers_SetTime(0, 5/timers_ms);/*se cumplen los 5ms asi que volvemos a recargar el mismo canal */
+            _7segments_Task();              /*actualiza el valor en el display y multiplexa al siguiente display*/
+        }
+        if(Timers_u16GetTime(1) == 0)/*preguntamos si la interrupcion decrmento hasta llegar a 0 el canal 1*/
+        {
+            Timers_SetTime(1, 100/timers_ms);       /*se cumplen los 200ms asi que volvemos a recargar el mismo canal */
+            u8Adc = Adc_u16Read( 3 );               /*se lee el canal del adc*/
+            u16Volts = (_U16)u8Adc * (_U16)13;      /*se trasnforma la lectura en milivolts*/
+            _7segments_SetNumber(u16Volts/10);      /*actualizamos el display con el valor en volts*/
+            u8Val = _7segments_u8GetDisplay(2);     /*se lee el tercer display*/
+            _7segments_SetDisplay(2, u8Val|0x80);   /*se agrega el punto decimal*/
+        }
+    }
+}
+
+
+#pragma interrupt YourHighPriorityISRCode
+void YourHighPriorityISRCode(void)
+{
+    /*coloca aquí el código que llevará tu interrupción en caso de usarla*/
+}
+
+#pragma interruptlow YourLowPriorityISRCode
+void YourLowPriorityISRCode(void)
+{
+    Timers_Isr();
+}
